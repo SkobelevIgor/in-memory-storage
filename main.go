de8cabb8533a1f6d10afb041c9ae5031de73ef61
igor <skobelev.igor@gmail.com>
2019-01-04 21:30:11 +0200

    start args, backup, load store

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ func main() {
 	var storeCfg store.Config
 	flag.IntVar(&storeCfg.BackupTimeSecs, "backup-interval", 0, "Backup store interval (secs)")
 	flag.IntVar(&storeCfg.BackupOps, "backup-ops-count", 0, "Backup each insert / update ops")
-	flag.BoolVar(&storeCfg.GracefulBackup, "graceful-backup", false, "Backup store on shutdown")
+	flag.BoolVar(&storeCfg.SkipGracefulBackup, "skip-backup-on-shutdown", false, "Backup store on shutdown")
 	flag.BoolVar(&storeCfg.SkipBackupLoad, "skip-backup-load", false, "Load store from backup")
 	flag.Parse()
 	err := store.InitStore(storeCfg)
--- store/store.go
+++ store/store.go
@@ -1,21 +1,29 @@
 package store
 
 import (
+	"encoding/gob"
 	"encoding/json"
+	"fmt"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 	"time"
 
 	uuid "github.com/satori/go.uuid"
 )
 
-const backupFile = ".store.gob"
+const (
+	backupFile     = ".store.gob"
+	backupFileTemp = ".store.go.TMP"
+)
 
 // Config store config
 type Config struct {
-	BackupTimeSecs int
-	BackupOps      int
-	GracefulBackup bool
-	SkipBackupLoad bool
+	BackupTimeSecs     int
+	BackupOps          int
+	SkipGracefulBackup bool
+	SkipBackupLoad     bool
 }
 
 // Record item in store
@@ -26,23 +34,26 @@ type Record struct {
 }
 
 type mainStore struct {
-	mx sync.RWMutex
-	s  map[string]Record
+	mx      sync.RWMutex
+	records map[string]Record
 }
 
-var store *mainStore
+var (
+	store    *mainStore
+	stopChan chan os.Signal
+)
 
 // InitStore initialize store, load from backup etc
 func InitStore(cfg Config) (err error) {
-	store = &mainStore{s: make(map[string]Record)}
+	store = &mainStore{records: make(map[string]Record)}
 	if cfg.BackupTimeSecs > 0 {
 		err = runBackupByTimer(cfg.BackupTimeSecs)
 	}
 	if err == nil && cfg.BackupOps > 0 {
 		err = runBackupByOpsCounter(cfg.BackupOps)
 	}
-	if err == nil && cfg.GracefulBackup == true {
-		// add panic processing
+	if err == nil && cfg.SkipGracefulBackup == false {
+		runStopListener()
 	}
 	if err == nil && cfg.SkipBackupLoad == false {
 		err = loadStoreFromBackup()
@@ -58,15 +69,52 @@ func runBackupByOpsCounter(opsCount int) error {
 	return nil
 }
 
-func loadStoreFromBackup() error {
-	return nil
+func loadStoreFromBackup() (err error) {
+	if _, err := os.Stat(backupFile); !os.IsNotExist(err) {
+		f, err := os.Open(backupFile)
+		if err == nil {
+			decoder := gob.NewDecoder(f)
+			recs := map[string]Record{}
+			err = decoder.Decode(&recs)
+			store.records = recs
+		}
+	}
+	return err
+}
+
+func backupStore() (err error) {
+	fmt.Println("Saving store to disk ...")
+	f, err := os.Create(backupFileTemp)
+	if err == nil {
+		defer f.Close()
+		enc := gob.NewEncoder(f)
+		enc.Encode(store.records)
+		err = os.Rename(backupFileTemp, backupFile)
+		fmt.Println("Store saved to disk")
+	}
+	return err
+}
+
+func runStopListener() {
+	stopChan = make(chan os.Signal)
+	signal.Notify(stopChan, syscall.SIGTERM)
+	signal.Notify(stopChan, syscall.SIGINT)
+	go func() {
+		signal := <-stopChan
+		fmt.Printf("caught sig: %+v\n", signal)
+		err := backupStore()
+		if err != nil {
+			fmt.Println(err)
+		}
+		os.Exit(0)
+	}()
 }
 
 // GetItemsCount returns total count of elements
 func GetItemsCount() (c int) {
 	store.mx.RLock()
 	defer store.mx.RUnlock()
-	c = len(store.s)
+	c = len(store.records)
 	return
 }
 
@@ -74,7 +122,7 @@ func GetItemsCount() (c int) {
 func GetRecord(id string) json.RawMessage {
 	store.mx.RLock()
 	defer store.mx.RUnlock()
-	rec, ok := store.s[id]
+	rec, ok := store.records[id]
 	if ok {
 		return rec.Data
 	}
@@ -91,7 +139,7 @@ func SaveRecord(data json.RawMessage) (id string) {
 	record := Record{ID: id, Data: data, CreatedAt: time.Now()}
 	store.mx.Lock()
 	defer store.mx.Unlock()
-	store.s[id] = record
+	store.records[id] = record
 	return
 }
 
@@ -99,10 +147,10 @@ func SaveRecord(data json.RawMessage) (id string) {
 func ReplaceRecord(id string, data json.RawMessage) (ok bool) {
 	store.mx.Lock()
 	defer store.mx.Unlock()
-	rec, ok := store.s[id]
+	rec, ok := store.records[id]
 	if ok {
 		rec.Data = data
-		store.s[id] = rec
+		store.records[id] = rec
 	}
 	return
 }
@@ -111,9 +159,9 @@ func ReplaceRecord(id string, data json.RawMessage) (ok bool) {
 func DeleteRecord(id string) bool {
 	store.mx.Lock()
 	defer store.mx.Unlock()
-	_, ok := store.s[id]
+	_, ok := store.records[id]
 	if ok {
-		delete(store.s, id)
+		delete(store.records, id)
 	}
 	return ok
 }
